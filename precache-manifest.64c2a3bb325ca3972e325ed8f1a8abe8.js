3bcd31918f390c9d2288a65f2f11dec7dcbdc353
tomsza26 <tomsza27@gmail.com>
2020-04-25 23:24:43 +0200

    Updates

---
+++ precache-manifest.64c2a3bb325ca3972e325ed8f1a8abe8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f84a1d273b3432d237a096d800dfcb3",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "3f307cf59ad7ae6a58d4",
+    "url": "/portfolio2/static/css/main.d794448e.chunk.css"
+  },
+  {
+    "revision": "88c0599ff87989d1084d",
+    "url": "/portfolio2/static/js/2.f4a0e47f.chunk.js"
+  },
+  {
+    "revision": "314e47395f0b3113fe113d7532b4cff0",
+    "url": "/portfolio2/static/js/2.f4a0e47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f307cf59ad7ae6a58d4",
+    "url": "/portfolio2/static/js/main.c71b8b9e.chunk.js"
+  },
+  {
+    "revision": "785e5a3dd06841f63b71",
+    "url": "/portfolio2/static/js/runtime-main.7d9b3d35.js"
+  },
+  {
+    "revision": "12a066cc921421ed8b46a6fe5b81dd4e",
+    "url": "/portfolio2/static/media/1.12a066cc.png"
+  },
+  {
+    "revision": "23e2b6cf75076c68a788053631d1f14d",
+    "url": "/portfolio2/static/media/ep_naturalblack.23e2b6cf.png"
+  },
+  {
+    "revision": "31f549b4a1627583895ee230df8d6645",
+    "url": "/portfolio2/static/media/fcc-Calculator.31f549b4.gif"
+  },
+  {
+    "revision": "24055dc0c99a863ac657cd0c6451b238",
+    "url": "/portfolio2/static/media/fcc-Drum_Machine.24055dc0.gif"
+  },
+  {
+    "revision": "0b1d5485e75e772b353a22c18ed42910",
+    "url": "/portfolio2/static/media/fcc-Pomodoro-Clock.0b1d5485.gif"
+  },
+  {
+    "revision": "646405d1f6e8b2c5e7fc9ce26fc91ef5",
+    "url": "/portfolio2/static/media/fcc-Quotes_Machine.646405d1.gif"
+  },
+  {
+    "revision": "58324ac5457f8efeaae0ab37cc98cf30",
+    "url": "/portfolio2/static/media/flipGame.58324ac5.gif"
+  },
+  {
+    "revision": "c90915f8110dcd8563ae13fa9d509514",
+    "url": "/portfolio2/static/media/markdown-notes.c90915f8.gif"
+  },
+  {
+    "revision": "0009a23baf76e54da6fc7cf8ed6f45d9",
+    "url": "/portfolio2/static/media/pakuj-z-tymkiem.0009a23b.gif"
+  },
+  {
+    "revision": "9601801b5d015aae25feda825f0e5ade",
+    "url": "/portfolio2/static/media/palm-tree.9601801b.png"
+  },
+  {
+    "revision": "eae9b665a4b93795ff81bc80e5f82382",
+    "url": "/portfolio2/static/media/portfolio.eae9b665.gif"
+  },
+  {
+    "revision": "751172c15355295c6024a3c658a4c416",
+    "url": "/portfolio2/static/media/tomekVHS.751172c1.JPG"
+  }
+]);
